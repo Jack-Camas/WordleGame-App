dd7b0888a344167ffb936e61169de1befac80edc
Jack Camas <jackcamasdev@gmail.com>
2023-11-25 17:05:00 -0500

    Add settings page

--- Wordle/OTHER/Constants.swift
+++ Wordle/OTHER/Constants.swift
@@ -8,3 +8,14 @@
 import Foundation
 
 let DELETE_KEY = "DEL"
+
+let kDeleteKey = "DEL"
+let kNumGuessesKey = "numGuesses"
+let kNumLettersKey = "numLetters"
+let kWordThemeKey = "wordTheme"
+let kIsAlienWordleKey = "isAlienWordle"
+
+let kMinGuesses = 3
+let kMaxGuesses = 8
+let kMinLetters = 4
+let kMaxLetters = 7
---
+++ Wordle/OTHER/SettingsViewController.swift
@@ -0,0 +1,16 @@
+//
+//  SettingsViewController.swift
+//  Wordle
+//
+//  Created by Jack Camas on 12/27/23.
+//
+
+import UIKit
+
+class SettingsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+}
